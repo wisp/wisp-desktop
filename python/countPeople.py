7241cea97dbaa509f280ff020e0265c8a95bf108
rmenon1008 <rmenon1008@gmail.com>
2022-08-12 14:45:53 -0700

    Removes occupancy counting for now

--- python/countPeople.py
+++ python/countPeople.py
@@ -1,16 +1,17 @@
 import cv2
-import tensorflow as tf
-import tensorflow_hub as hub
 import numpy as np
 import base64
-import eel
+# import eel
 
-DO_OCCUPANCY_COUNT = False
+# DO_OCCUPANCY_COUNT = False
 
-if (DO_OCCUPANCY_COUNT):
-    print("Loading TF model...")
-    detector = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
-    print("Done loading TF model")
+# if (DO_OCCUPANCY_COUNT):
+#     import tensorflow as tf
+#     import tensorflow_hub as hub
+
+#     print("Loading TF model...")
+#     detector = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
+#     print("Done loading TF model")
 
 def increase_brightness(img, value):
     img_brightened = np.where((255 / img) < value,255,(img*value).astype("uint8"))
@@ -33,29 +34,31 @@ def count_people(image):
     image = cv2.inpaint(image, missing_mask, 3, cv2.INPAINT_TELEA)
     image_gray = image.copy()
 
-    if (DO_OCCUPANCY_COUNT):
-        # STEP 3: Convert grayscale to 3 channel RGB for the detector
-        #         Reshape to match the detector input
-        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-        (w, h, c) = image.shape
-        image = tf.convert_to_tensor(image)
-        image = tf.reshape(image, [1,w,h,3])
+    count = 0
 
-        # STEP 4: Run the detector
-        #         Draw bounding boxes around the detected objects
-        results = detector(image)
-        detections = len(results["detection_classes"][0])
-        count = 0
-        for i in range(detections):
-            if results["detection_classes"][0][i] == 1:
-                # print("Person detected with confidence: " + str(results["detection_scores"][0][i]))
-                if results["detection_scores"][0][i] > 0.40:
-                    count += 1
-                    rect = results["detection_boxes"][0][i].numpy()
-                    start = (int(rect[1] * w), int(rect[0] * h))
-                    end = (int(rect[3] * w), int(rect[2] * h))
-                    image_gray = cv2.rectangle(image_gray, start, end, 255, 1)
-        print("Count: {}".format(count))
+    # if (DO_OCCUPANCY_COUNT):
+    #     # STEP 3: Convert grayscale to 3 channel RGB for the detector
+    #     #         Reshape to match the detector input
+    #     image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+    #     (w, h, c) = image.shape
+    #     image = tf.convert_to_tensor(image)
+    #     image = tf.reshape(image, [1,w,h,3])
+
+    #     # STEP 4: Run the detector
+    #     #         Draw bounding boxes around the detected objects
+    #     results = detector(image)
+    #     detections = len(results["detection_classes"][0])
+    #     count = 0
+    #     for i in range(detections):
+    #         if results["detection_classes"][0][i] == 1:
+    #             # print("Person detected with confidence: " + str(results["detection_scores"][0][i]))
+    #             if results["detection_scores"][0][i] > 0.40:
+    #                 count += 1
+    #                 rect = results["detection_boxes"][0][i].numpy()
+    #                 start = (int(rect[1] * w), int(rect[0] * h))
+    #                 end = (int(rect[3] * w), int(rect[2] * h))
+    #                 image_gray = cv2.rectangle(image_gray, start, end, 255, 1)
+    #     print("Count: {}".format(count))
 
     buffer = cv2.imencode('.png', image_gray)[1]
     b64 = base64.b64encode(buffer)
