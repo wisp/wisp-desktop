9dbf01a405f977673e739a64cecf1a91fdedb199
rmenon1008 <rmenon1008@gmail.com>
2023-02-27 12:27:00 -0800

    Build script options

--- build_app.py
+++ build_app.py
@@ -2,6 +2,13 @@ import os
 import shutil
 import subprocess
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Build the WISP Desktop app.')
+parser.add_argument('--no-react', action='store_true', help='Do not build the react app')
+parser.add_argument('--no-python', action='store_true', help='Do not build the python app')
+parser.add_argument('--usb', action='store_true', help='Copy the dist folder to a USB drive')
+args = parser.parse_args()
 
 def runCommand(command):
     return_value = os.system(command)
@@ -9,9 +16,6 @@ def runCommand(command):
         raise Exception("Command failed: " + command)
 
 starting_dir = os.getcwd()
-
-no_react = len(sys.argv) > 1 and "no-react" in sys.argv[1]
-no_python = len(sys.argv) > 1 and "no-python" in sys.argv[1]
 
 try: 
     # Create a copy of the dist folder if it exists
@@ -26,7 +30,7 @@ try:
     if os.path.exists("dist"):
         shutil.rmtree("dist")
 
-    if not no_react:
+    if not args.no_react:
         # Build the react UI to its build folder
         print(">> Building the react app")
         os.chdir("react")
@@ -39,16 +43,27 @@ try:
             shutil.rmtree("python/web")
         shutil.move("react/build/", "python/web/")
 
-    if not no_python:
+    if not args.no_python:
         # Build the python app to the dist folder using PyInstaller
         print("Building the python app with PyInstaller")
         os.chdir("python")
-        os.system("python3 -m eel main.py web --paths ./sllurp --windowed --onefile --name WISP_Desktop --icon ../react/public/logo.ico")
+        os.system("python -m eel main.py web --paths ./sllurp --windowed --onefile --name WISP_Desktop --icon ../react/public/logo.ico")
         os.chdir("..")
 
         # Moving the built app into the dist folder
         print(">> Moving the built app into the dist folder")
-        shutil.move("python/dist", "./")
+        shutil.copytree("python/dist", "./")
+
+    if args.usb:
+        # Copy the dist folder to a USB drive
+        # Check if the USB drive is connected
+        if os.path.exists("D:/"):
+            print(">> Copying the dist folder to the USB drive")
+            if os.path.exists("D:/WISP_Desktop"):
+                shutil.rmtree("D:/WISP_Desktop")
+            shutil.copytree("dist", "D:/WISP_Desktop")
+        else:
+            print(">> USB drive not found")
 
     # Deleting the backup of the dist folder
     if os.path.exists('.dist_backup'):
